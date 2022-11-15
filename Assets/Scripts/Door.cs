82055b4cf473851c4d9f16d4143d738275fa162d
Beadhangerone <denm.2206@gmail.com>
2022-11-15 19:19:47 +0100

    security room + Spaceship room

---
+++ Assets/Scripts/Door.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door : MonoBehaviour
+{
+    private Animator _animator;
+    public GameObject model;
+
+    private static readonly int DoorOpen = Animator.StringToHash("DoorOpen");
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Open()
+    {
+        //Destroy(model); // LAME
+        _animator.SetTrigger(DoorOpen);
+    }
+}
---
+++ Assets/Scripts/KeyPad.cs
@@ -0,0 +1,86 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class KeyPad : MonoBehaviour
+{
+    public GameObject display;
+    public Trigerable Trigerable;
+    public string code;
+    
+    private TextMeshPro displayText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        displayText = display.GetComponent<TextMeshPro>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void AppendNr(int nr)
+    {
+        string text = displayText.text;
+        string newText = text.Substring(1) +""+nr;
+        displayText.text = newText;
+        if (newText == code)
+        {
+            Trigerable.onTrigger.Invoke();
+        }
+    }
+
+    public void OnPress1()
+    {
+        AppendNr(1);
+    }
+    
+    public void OnPress2()
+    {
+        AppendNr(2);
+    }
+    
+    public void OnPress3()
+    {
+        AppendNr(3);
+    }
+    
+    public void OnPress4()
+    {
+        AppendNr(4);
+    }
+    
+    public void OnPress5()
+    {
+        AppendNr(5);
+    }
+    
+    public void OnPress6()
+    {
+        AppendNr(6);
+    }
+    
+    public void OnPress7()
+    {
+        AppendNr(7);
+    }
+    
+    public void OnPress8()
+    {
+        AppendNr(8);
+    }
+    
+    public void OnPress9()
+    {
+        AppendNr(9);
+    }
+    
+    public void OnPress0()
+    {
+        AppendNr(0);
+    }
+}
