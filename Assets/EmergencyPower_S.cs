ffaa9dbbaf404fd1a1d67348dd74b9534c03085d
leldenor <norenberga19@outlook.lv>
2022-11-15 11:40:41 +0100

    some color

--- Assets/EmergencyPower_S.cs
+++ Assets/EmergencyPower_S.cs
@@ -9,20 +9,30 @@ public class EmergencyPower_S : MonoBehaviour
 
     [SerializeField] private float EventAngle = 45;
     [SerializeField] private float SpawnAngle = 0;
+    public Light light;
+    public Light light2;
     private GameObject rotator;
+    private Wire_S wires;
     
     // Start is called before the first frame update
     void Start()
     {
-        rotator = GameObject.Find("rotator");
+        rotator = GameObject.Find("rotatoBaser");
         GetComponentInChildren(typeof(GameObject)).transform.Rotate(0,SpawnAngle,0);
     }
 
     private void OnTransformChildrenChanged()
     {
-        if(rotator.gameObject.transform.up.y == EventAngle)
+        if(rotator.transform.up.y == EventAngle && wires.ObjectsConnected > 1)
         {
-            rotator.gameObject.BroadcastMessage("AllFiredUp", SendMessageOptions.RequireReceiver);
+            light.enabled = true;
+
+            light2.color = new Color(241, 212, 133);
+        }
+        else if (rotator.transform.up.y == -45 && wires.ObjectsConnected > 1)
+        {
+            light.enabled = false;
+            light2.color = new Color(115, 115, 113);
         }
     }
 
--- Assets/Wire_S.cs
+++ Assets/Wire_S.cs
@@ -8,33 +8,45 @@ using UnityEngine;
 public class Wire_S : MonoBehaviour
 {
 
-    private Vector3 OriginalPosition;
+    private Vector3 OriginalPosition1;
+    private Vector3 OriginalPosition2;
 
-    private GameObject obj;
+    public GameObject obj1;
+    public GameObject obj2;
+
+    public int ObjectsConnected { get; private set; }
     // Start is called before the first frame update
     void Start()
     {
-        GameObject.Find("Wire").transform.right = OriginalPosition;
-        obj = GameObject.Find("Wire");
+        OriginalPosition1 = obj1.transform.right;
+        OriginalPosition2 = obj2.transform.right;
+        ObjectsConnected = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
-        Vector3 otp = obj.transform.position;
-        Vector3 newPos = otp - OriginalPosition;
-        Vector3 newScale = new Vector3(0.1f, otp.y - OriginalPosition.y, 0.1f);
-        obj.transform.localScale = newScale;
+        Vector3 otp = obj1.transform.position;
+        Vector3 newPos = otp - OriginalPosition1;
+        Vector3 newScale = new(0.1f, otp.y - OriginalPosition1.y, 0.1f);
+        obj1.transform.localScale = newScale;
 
+        Vector3 scale2 = new(0.1f, obj2.transform.position.y - OriginalPosition2.y, 0.1f);
     }
 
     private void OnCollisionEnter(Collision other)
     {
         if (other.gameObject.name == "WireCol")
         {
-            obj.GetComponentInParent<HandGrabInteractable>().enabled = false;
-            obj.GetComponentInParent<Grabbable>().enabled = false;
-            obj.gameObject.BroadcastMessage("FlipTheSwitch");
+            obj1.GetComponentInParent<HandGrabInteractable>().enabled = false;
+            obj1.GetComponentInParent<Grabbable>().enabled = false;
+            ObjectsConnected++;
+        } 
+        else if (other.gameObject.name == "WireCol2")
+        {
+            obj2.GetComponentInParent<HandGrabInteractable>().enabled = false;
+            obj2.GetComponentInParent<Grabbable>().enabled = false;
+            ObjectsConnected++;
         }
     }
 }
